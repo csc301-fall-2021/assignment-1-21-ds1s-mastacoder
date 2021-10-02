2f5fe8065cf3bad7108c003efa63e9b05a2af58b
Daniel Saunders <daniel.saunders@mail.utoronto.ca>
2021-10-02 01:59:20 -0400

    fixed consistency bug with products

--- server/api/products/product_service.py
+++ server/api/products/product_service.py
@@ -1,14 +1,17 @@
 from typing import Union
 from fastapi import HTTPException
 from api.products.pseudo_db import products, discounts
-from api.products.models import ProductsResponseModel, NameOrUid, DiscountsResponseModel
+from api.products.models import ProductResponseModel, ProductsResponseModel, NameOrUid, DiscountsResponseModel
 from api.settings import settings
 
 
 def get_all_products() -> ProductsResponseModel:
+    modified_products = []
     for product in products:
-        product.img_url = settings.IMAGE_SERVER_BASE_URL + product.img_url
-    return ProductsResponseModel(products=products)
+        img_adjusted_product = ProductResponseModel(**product.dict())
+        modified_products.append(img_adjusted_product)
+        img_adjusted_product = settings.IMAGE_SERVER_BASE_URL + img_adjusted_product.img_url
+    return ProductsResponseModel(products=modified_products)
 
 def get_product(identifier: NameOrUid, value_to_identify: Union[str, int]):
     for product in products:
